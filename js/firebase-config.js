29c1c447a55dfc595628a0ae6a15983428d08b79
Meghashyam Bhandary <r21meghashyam@gmail.com>
2019-05-29 02:50:16 +0530

    major

---
+++ js/firebase-config.js
@@ -0,0 +1,10 @@
+//Initialize Firebase
+const config = {
+  apiKey: "AIzaSyAISWZjL7FbC64iPTx78DZ_08wQQW-EAeI",
+  authDomain: "facerecognition-megh.firebaseapp.com",
+  databaseURL: "https://facerecognition-megh.firebaseio.com",
+  projectId: "facerecognition-megh",
+  storageBucket: "gs://facerecognition-megh.appspot.com",
+  messagingSenderId: "754202674833"
+};
+firebase.initializeApp(config);
